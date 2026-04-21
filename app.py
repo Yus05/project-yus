12a02a987708ff1569ca23ffefca1cee7be7ab9f
Yus <yusdev05@gmail.com>
2026-04-21 17:18:48 -0400

    add: endpoints de prueba

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
-    return "Hola"
+    return "hola"
+    
+@app.route("/list")
+def list():
+    return jsonify({
+        "contenido": "list"
+    })
+    
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
