ba2471312d9239c3fb0ebe40a3f6296450f394ad
Yus <yusdev05@gmail.com>
2026-04-21 18:15:49 -0400

    Asegurando cambios de endpoints

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+usuarios = ["Ana", "Betto", "Carla"]
+
+@app.route("/usuarios")
+def obtener_usuarios():
+    return {"lista_usuarios": usuarios}
+
+
+
+@app.route('/buzon', methods=['GET', 'POST'])
+def buzon():
+    if request.method == 'GET':
+        return "Este es el formulario. Usa POST para enviar datos."
+
+    if request.method == 'POST':
+        # Sacamos los datos que enviaste
+        nombre = request.json['nombre']
+        return f"Hola {nombre}, recibí tu formulario"
+
+
 @app.route("/")
 def index():
-    return "hola"
-    
-@app.route("/list")
-def list():
-    return jsonify({
-        "contenido": "list"
-    })
-    
+    return render_template('index.html')
 
     
 if __name__ == "__main__":
